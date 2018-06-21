4927c388e1dcdbf996867165525ad081d49f6b3e
Cristian Adam <cristianradam@gmail.com>
2018-06-21 18:49:27 -0400

    added msg files to cmakelists & uncommented code to fix

--- src/AVC_path_planner.cpp
+++ src/AVC_path_planner.cpp
@@ -1,8 +1,18 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
-//#include "geometry_msgs/Twist.msg"
+#include "geometry_msgs/Twist.h"
+#include "sensor_msgs/LaserScan.h"
+
+#include "mdart/Path.h"
 
 #include <sstream>
+
+void newLidarDataCallback(const sensor_msgs::LaserScan::ConstPtr& scan)
+{
+    // TODO: use this data, currently writing this node as a stub to test compiling/publishing/subscribing
+    // http://docs.ros.org/api/sensor_msgs/html/msg/LaserScan.html
+    ROS_INFO("PathGenerator Received LaserScan: [%f]", scan->ranges[539]);
+}
 
 int main(int argc, char **argv)
 {
@@ -11,11 +21,11 @@ int main(int argc, char **argv)
 
   ros::NodeHandle nodeHandle;
 
-//  ros::Publisher path_pub = nodeHandle.advertise<mdart::Path>("da_wae", 1);
+  ros::Publisher path_pub = nodeHandle.advertise<mdart::Path>("da_wae", 1);
   // last parameter: queue depth of 1, "low" depth as this info will probably become stale relatively fast
   // Q: if the queue fills, messages seem to be discarded - will it discard the oldest message from the queue or the new one attempted to be added to the queue?
 
-//    ros::Subscriber subscriber = nodeHandle.subscribe("lidar_in", 1, newLidarDataCallback);
+    ros::Subscriber subscriber = nodeHandle.subscribe("lidar_in", 1, newLidarDataCallback);
 
   ros::Rate loop_rate(50);
   // 50 hz publish rate
@@ -26,7 +36,7 @@ int main(int argc, char **argv)
     ros::spinOnce();
     // calls newLidarDataCallback if there are messages in it's queue to be run
 
-//    geometry_msgs::Twist path;
+    geometry_msgs::Twist path;
 
     /*
     float64 speed
@@ -42,7 +52,7 @@ int main(int argc, char **argv)
 //msg.linear.x = double(rand())/double(RAND_MAX); // vehicle speed in meters per second
 //msg.angular.z = 2*double(rand())/double(RAND_MAX) - 1; // vehicle rotation in radians per second
 
-//	path_pub.publish(path);
+	path_pub.publish(path);
 
 
 
@@ -54,11 +64,5 @@ int main(int argc, char **argv)
   return 0;
 }
 
-/*
-void newLidarDataCallback(const sensor_msg::LaserScan::ConstPtr& scan)
-{
-    // TODO: use this data, currently writing this node as a stub to test compiling/publishing/subscribing
-    // http://docs.ros.org/api/sensor_msgs/html/msg/LaserScan.html
-    ROS_INFO("PathGenerator Received LaserScan: [%s]", scan->ranges[539].c_str());
-}
-*/
+
+
